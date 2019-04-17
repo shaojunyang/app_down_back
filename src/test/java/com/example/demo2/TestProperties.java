2e58e5ce1987d8641ee92f92f407318f09017fe9
shaojunyang <yang152389@139.com>
2019-04-17 12:06:37 +0800

    'mq'

--- src/test/java/com/example/demo2/TestProperties.java
+++ src/test/java/com/example/demo2/TestProperties.java
@@ -1,11 +1,9 @@
 package com.example.demo2;
 
 import com.example.demo2.config.HostProperties;
-import com.example.demo2.mq.HelloSender;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
